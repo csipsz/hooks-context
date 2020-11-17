16f56631297225a13d95d84ebb105655c616406b
Luca <lucakanyo@protonmail.com>
2020-11-16 21:59:00 -0500

    added Context Provider to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navbar from './components/Navbar';
 import { ProjectList } from './components/ProjectList';
+import ThemeContextProvider from './contexts/ThemeContext';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <ProjectList/>
+      <ThemeContextProvider>
+        <Navbar/>
+        <ProjectList/>
         <img src={logo} className="App-logo" alt="logo" />
+      </ThemeContextProvider>
     </div>
   );
 }
