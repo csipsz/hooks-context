97f9ea856764ee437df2f65c2d54e330b3a72e15
Luca <lucakanyo@protonmail.com>
2020-11-18 12:22:51 -0500

    destructuring context in nav and projectlist components

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export class ProjectList extends Component {
+    static contextType = ThemeContext
     render() {
+        const {isLightTheme, light, dark} = this.context;
+        const theme = isLightTheme ? light : dark
         return (
-            <div className='project-list'>
+            <div className='project-list' style={{color: theme.syntax, background: theme.bg}}>
                 <ul>
-                    <li>CLI</li>
-                    <li>Sinatra</li>
-                    <li>Rails</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
+                    <li style={{background: theme.ui}}>CLI</li>
+                    <li style={{background: theme.ui}}>Sinatra</li>
+                    <li style={{background: theme.ui}}>Rails</li>
+                    <li style={{background: theme.ui}}>JavaScript</li>
+                    <li style={{background: theme.ui}}>React</li>
                 </ul>
             </div>
         )
