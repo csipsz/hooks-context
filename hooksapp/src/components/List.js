28dc0027d9653165d266ce8a50af43ff3c18e87e
Luca <lucakanyo@protonmail.com>
2020-11-18 13:43:49 -0500

    deleted extra files

---
+++ hooksapp/src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div className="list">
+            <ul>
+                <li>Memory</li>
+                <li>Hi</li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default List
