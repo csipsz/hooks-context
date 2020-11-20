92425611b291c5646143a38b7046f1fd6b190a0c
Luca <lucakanyo@protonmail.com>
2020-11-20 11:02:47 -0500

    added useContext to use the ThemeContext in Projectlist

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export class ProjectList extends Component {
-    static contextType = ThemeContext
-    render() {
-        const {isLightTheme, light, dark} = this.context;
-        const theme = isLightTheme ? light : dark
-        return (
-            <div className='project-list' style={{color: theme.syntax, background: theme.bg}}>
+// export class ProjectList extends Component {
+//     static contextType = ThemeContext
+//     render() {
+//         const {isLightTheme, light, dark} = this.context;
+//         const theme = isLightTheme ? light : dark
+//         return (
+//             <div className='project-list' style={{color: theme.syntax, background: theme.bg}}>
+//                 <ul>
+//                     <li style={{background: theme.ui}}>CLI</li>
+//                     <li style={{background: theme.ui}}>Sinatra</li>
+//                     <li style={{background: theme.ui}}>Rails</li>
+//                     <li style={{background: theme.ui}}>JavaScript</li>
+//                     <li style={{background: theme.ui}}>React</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+export const ProjectList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark
+    return (
+        <div className='project-list' style={{color: theme.syntax, background: theme.bg}}>
                 <ul>
                     <li style={{background: theme.ui}}>CLI</li>
                     <li style={{background: theme.ui}}>Sinatra</li>
@@ -16,8 +33,8 @@ export class ProjectList extends Component {
                     <li style={{background: theme.ui}}>React</li>
                 </ul>
             </div>
-        )
-    }
+    )
 }
 
 export default ProjectList
+
