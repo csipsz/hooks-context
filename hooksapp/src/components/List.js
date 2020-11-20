31c048852b9c12ea68398ddc76e0a12a1d40fdd7
Luca <lucakanyo@protonmail.com>
2020-11-19 19:04:16 -0500

    added useEffect to project

--- hooksapp/src/components/List.js
+++ hooksapp/src/components/List.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import NewSongForm from './NewSongForm';
 
 const List = () => {
@@ -7,10 +7,16 @@ const List = () => {
         {title: 'getting away with murder', id: 2}
 
     ]);
-
+    const [age, setAge] = useState(20);
     const addSong = (title) => {
         setSongs([...songs, {title, id: Math.random()}])
     }
+    useEffect(() => {
+        console.log('useeffect hook ran', songs)
+    }, [songs])
+    useEffect(() => {
+        console.log('useeffect hook ran', age)
+    }, [age])
     return (
         <div className="list">
             <ul>
@@ -19,6 +25,7 @@ const List = () => {
                     })}
             </ul>
             <NewSongForm addSong={addSong}/>
+            <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
         </div>
     )
 }
